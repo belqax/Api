8f1f805c251d3251c51bb82703e34d3d0668c304
belqax <Ron.der.4u@gmail.com>
2025-12-14 23:11:59 +0700

    .

--- app/config.py
+++ app/config.py
@@ -22,7 +22,7 @@ class Settings(BaseSettings):
     # JWT
     jwt_secret_key: str
     jwt_algorithm: str = "HS256"
-    access_token_expire_minutes: int = 60
+    access_token_expire_minutes: int = 10
     refresh_token_expire_days: int = 30
 
     # Верификация e-mail
